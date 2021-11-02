b13945c2b0c91aa8d650263657623682aa448025
UntillNIGHT <lengleng9090@gmail.com>
2021-11-02 23:22:25 +0700

    [UPDATE] add explanation in file string2int.js

--- string2int.js
+++ string2int.js
@@ -1,34 +1,34 @@
-function string2int(word) {
+function string2int(word) { 
     let newword = '';
-    let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0] //create variable "nums" for one digit static number.
 
     let wordLength = 0;
     while (word[wordLength] !== undefined) {
-        wordLength++;
+        wordLength++; //loop to get length of word.
     }
 
     for (let i = 0; i < wordLength; i++) {
         for (let j = 0; j < 10; j++) {
             if (word[i] == nums[j]) {
-                newword += word[i]
+                newword += word[i] //use nested loop to filter get only string number. 
             }
         }
     }
 
     let result = 0;
-    let newWordLength = 0;
-    let digit = 0;
+    let newWordLength = 0; 
+    let digit = 0; //create variable "digit" for assign place value of newWord.
 
     while (newword[newWordLength] !== undefined) {
-        newWordLength++;
+        newWordLength++; //use loop to get a length of newWord 
     }
-    digit = newWordLength - 1;
+    digit = newWordLength - 1; //assign (newWordLength - 1) to digit variable.
 
-    for (let i = 0; i < newWordLength; i++) {
-        switch (newword[i]) {
-            case "1":
-                result += 1 * (10 ** digit);
-                digit--;
+    for (let i = 0; i < newWordLength; i++) { 
+        switch (newword[i]) { //use loop to check each letter. if letter match in condition then calculate result.
+            case "1": 
+                result += 1 * (10 ** digit); //the result calculate by using power of 10 to keep each number in the correct place value.
+                digit--; //decrease "digit" for use in next newWord letter.
                 break;
             case "2":
                 result += 2 * (10 ** digit);
@@ -70,6 +70,6 @@ function string2int(word) {
     return result;
 }
 
-console.log(string2int("abc5703"));
+console.log(string2int("abc573"));
 
 console.log(string2int("a5b7c3"));
